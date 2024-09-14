6931d3d3cf0e825074c685a236465c44ed8c099d
Ukaypromise <promiseukaimo@gmail.com>
2024-09-14 18:43:57 +0100

    Add more test

--- spec/requests/graphql/mutations/sign_in_user_spec.rb
+++ spec/requests/graphql/mutations/sign_in_user_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 RSpec.describe 'GraphQL User SignIn Mutation', type: :request do
   let!(:user) { create(:user, email: "beca@gmail.com", password: "123456") }
-  it 'should sign in a user with the correct credentials' do
-    mutation = <<~MUTATION
+
+  let(:mutation) do
+    <<~MUTATION
       mutation($input: SignInUserInput!) {
         signInUser(input: $input) {
           user {
@@ -15,22 +16,32 @@ RSpec.describe 'GraphQL User SignIn Mutation', type: :request do
         }
       }
       MUTATION
+  end
 
-    user_params = {
+  def sign_in_user(email:, password:)
+      user_params = {
         credentials: {
-          email: "beca@gmail.com",
-          password: "123456"
+          email: email,
+          password: password
         }
     }
 
     post "/graphql", params: { query: mutation, variables: { input: user_params } }
+  end
 
+  it 'should sign in a user with the correct credentials' do
+    sign_in_user(email: "beca@gmail.com", password: "123456")
     expect(response.parsed_body["errors"]).to be_nil
 
     expect(response.parsed_body["data"]["signInUser"]["token"]).to be_present
 
     expect(response.parsed_body["data"]["signInUser"]["user"]["email"]).to eq(
-      user_params[:credentials][:email]
+      "beca@gmail.com"
     )
+  end
+
+  it "should not sign in a user with incorrect credentials" do
+    sign_in_user(email: "", password: "")
+    expect(response.parsed_body["errors"]).to be_present
   end
 end
