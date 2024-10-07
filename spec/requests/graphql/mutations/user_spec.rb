e8adf8579f41b6dbee5b61d4e49c646136dde03b
Ukaypromise <promiseukaimo@gmail.com>
2024-10-07 09:51:15 +0100

    Remove rails logsgers

--- spec/requests/graphql/mutations/user_spec.rb
+++ spec/requests/graphql/mutations/user_spec.rb
@@ -27,7 +27,6 @@ RSpec.describe 'GraphQL User Mutation', type: :request do
 
     expect {
       post "/graphql", params: { query: mutation, variables: { input: user_params } }
-      puts "#{response.parsed_body}"
     }.to change { User.count }.by(1)
 
     expect(response.parsed_body["errors"]).to be_blank
